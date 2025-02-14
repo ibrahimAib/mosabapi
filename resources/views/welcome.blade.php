c6dbde3ae3cc199bb581a03c731400a72d9689a7
“ibrahim <e1415e@gmail.com>
2025-02-14 03:27:15 +0300

    welcom page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <?php echo "hello wordl" ?>
+    <img height="100%" src="https://pfst.cf2.poecdn.net/base/image/f7a9cd7b076c7f7c0f6c127a9eeb5fe2365ebf6c34eed184103a97c3f939bc5c?w=1024&h=768&pmaid=286122813" alt="" srcset="">
+</body>
+</html>
